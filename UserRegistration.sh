f714709387f6dd74e4a321259d4a44fffe250619
Uthsavikp <uthsavi29@gmail.com>
2020-11-06 03:20:56 +0530

    Added:To Enter Valid First Name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,1 +1,13 @@
+#!/bin/bash -x
 echo " welcome "
+
+#User Needs To Enter Valid First Name
+read -p "Enter your first name - " firstName
+pattern="^[A-Z]{1}[a-zA-Z]{2,}$";
+
+if [[ $firstName =~ $pattern ]]
+then
+        echo "valid"
+else
+        echo "invalid"
+fi
